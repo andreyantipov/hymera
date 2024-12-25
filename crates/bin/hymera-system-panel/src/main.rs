d296bd68dc7cdef8bdb28196d2c48f3ae3fa517c
--local <--local>
2024-12-25 19:22:28 +0000

    refactor

--- crates/bin/hymera-system-panel/src/main.rs
+++ crates/bin/hymera-system-panel/src/main.rs
@@ -9,10 +9,10 @@ use ui::Panel;
 
 fn main() {
     let panel_window: WindowBuilder = window::create();
+    let cfg = dioxus::desktop::Config::new();
 
     LaunchBuilder::new()
-        .with_cfg(dioxus::desktop::Config::new()
-        .with_window(panel_window))
+        .with_cfg(cfg.with_window(panel_window))
         .launch(app);
 }
 
